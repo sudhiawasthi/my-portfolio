c2e841c3f4cfcf82ea19c9eb3462407c35649a12
Sudhi Awasthi <umangj11@gmail.com>
2020-07-19 03:00:43 +0530

    Add JSON and parse it

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -14,6 +14,8 @@
 
 package com.google.sps.servlets;
 
+import com.google.gson.Gson;
+import java.util.ArrayList; 
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -26,9 +28,25 @@ public class DataServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+    // List to show on the page
+    ArrayList<String> messages = new ArrayList<String>();
     
-    String content = "I am sent by the server via the servlet";
+      messages.add("This section will convert to comment1");
+      messages.add("This section will convert to comment2");
+      messages.add("This section will convert to comment3");
+
+    //Convert list to JSON 
+    String json = convertToJson(messages);
+
     response.setContentType("text/html;");
-    response.getWriter().println(content);
+    response.getWriter().println(json);
+  }
+
+  private String convertToJson(ArrayList<String> messages)
+  {
+    Gson gson = new Gson();
+    String json = gson.toJson(messages);
+    return json;
   }
 }
